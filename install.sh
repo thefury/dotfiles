3903cf94287032b1c314ab5cfefb357572f313f1
Trevor Oke <toke@kineticcafe.com>
2019-05-08 14:18:54 -0400

    Add spacemacs config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in bash_profile bashrc bash_aliases vimrc vim zshrc tmux.conf taskrc jrnl_config aliases.d
+for f in bash_profile bashrc bash_aliases vimrc vim zshrc tmux.conf taskrc jrnl_config aliases.d spacemacs
 do
   echo "processing $f"
   rm -r ~/.$f
