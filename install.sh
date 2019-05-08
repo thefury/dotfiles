48c10914329714af405ef717e12aa69926673345
Trevor Oke <toke@kineticcafe.com>
2019-05-08 12:57:45 -0400

    Add emacs config for spacemacs

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in bash_profile bashrc bash_aliases vimrc vim zshrc tmux.conf taskrc jrnl_config aliases.d
+for f in bash_profile bashrc bash_aliases emacs.d vimrc vim zshrc tmux.conf taskrc jrnl_config aliases.d
 do
   echo "processing $f"
   rm -r ~/.$f
