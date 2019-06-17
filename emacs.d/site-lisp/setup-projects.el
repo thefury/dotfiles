953cbbbcdead6274a42d05aa8a84056cc35044e2
Trevor Oke <toke@kineticcafe.com>
2019-06-17 14:33:17 -0400

    Add shortcuts for projectile

--- emacs.d/site-lisp/setup-projects.el
+++ emacs.d/site-lisp/setup-projects.el
@@ -4,6 +4,8 @@
   (setq projectile-require-project-root nil)
   :config
   (projectile-mode 1)
-  (setq projectile-project-search-path '("~/dev")))
+  (setq projectile-project-search-path '("~/dev"))
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 (provide 'setup-projects)
