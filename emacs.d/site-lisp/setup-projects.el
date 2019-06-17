0c57d19c29e55c8b21ae6fd2e2a02f2b5c747bf0
Trevor Oke <toke@kineticcafe.com>
2019-06-17 14:50:37 -0400

    Fix projectile

--- emacs.d/site-lisp/setup-projects.el
+++ emacs.d/site-lisp/setup-projects.el
@@ -1,9 +1,8 @@
 (use-package projectile
   :ensure t
   :init
-  (setq projectile-require-project-root nil)
   :config
-  (projectile-mode 1)
+  (projectile-global-mode)
   (setq projectile-project-search-path '("~/dev"))
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
