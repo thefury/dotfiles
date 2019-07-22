bf80763256b175d4149d35adcbca9d67e3c6f3ca
Trevor Oke <trevor@trevoroke.com>
2019-07-22 10:54:58 -0400

    Add yasnippet

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -64,3 +64,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas-global-mode 1)
